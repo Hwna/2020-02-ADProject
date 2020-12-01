0b2c266f6d3533a32c51b1968ca43ea5afb3f033
miten267 <66051620+miten267@users.noreply.github.com>
2020-12-01 11:20:26 +0900

    환전 방식 바꿈, 전일 대비 변화율 추가
    전일 대비 변화율을 추가했는데 기준 시간이 중부 유럽시 오후 3시다.... ㅠㅠ 네이버 환율보다 업데이트가 9시간 느리다.. 그래서 정보의 차이가 있다..
    이거 넣은 이유는 저번 시간에 안오셨을때 교수님이 네이버 환율 보시고 이런 기능도 넣으면 좋겠다고 말씀하셔서 만들어 봤습니다.

--- exchange.py
+++ exchange.py
@@ -1,28 +1,51 @@
 #exchange
 from forex_python.converter import CurrencyRates, CurrencyCodes
+from datetime import datetime, timedelta
 
 
 class calcExchange:
-    def __init__(self):
+    def __init__(self, nation1, nation2):
         self.c = CurrencyRates()
         self.s = CurrencyCodes()
+        self.nation1 = nation1
+        self.nation2 = nation2
 
-    def getRate(self, nation1, nation2):        #환율 추출
-        self.rate = (self.c).get_rate(nation1, nation2)
+    def getRate(self):        #환율 추출
+        self.rate = (self.c).get_rate(self.nation1, self.nation2)
         return self.rate
 
     def calculate(self, beforeCalc):        #입력받은 값 환전
-        self.afterCalc = round((self.rate * beforeCalc),2)
-        return self.afterCalc
+        self.beforeCalc = beforeCalc
+        self.afterCalc = (self.c).convert(self.nation1, self.nation2, beforeCalc)
+        return round(self.afterCalc,2)
 
     def getIcon(self, nation):      #화폐 기호 가져오기
         self.icon = (self.s).get_symbol(nation)
         return self.icon
 
+    def getChange(self, nation):        #전일 대비 변화율 구하기, The rates are updated daily 3PM CET.
+        self.yesterday = datetime.today() - timedelta(2)
+        self.todayTsr = (self.c).convert(nation, 'KRW', 1)
+        self.yesterdayTsr = (self.c).convert(nation, 'KRW', 1, self.yesterday)
+        self.tsrChange = self.todayTsr - self.yesterdayTsr
+        self.changeRate = 100 - (self.yesterdayTsr)/(self.todayTsr) * 100
+        self.tsrChange = round((self.tsrChange),2)
+        self.changeRate = round((self.changeRate),2)
+        if(self.tsrChange > 0):
+            self.code = '▲'
+        elif(self.tsrChange < 0):
+            self.code = '▼'
+        else :
+            self.code = '-'
+        self.changeChart = (self.code + " " + str(self.tsrChange)+ " / " + str(self.changeRate) + "%")
+        return self.changeChart
+
+        
+
 if __name__ == '__main__':
-    cal1 = calcExchange()
     s1 = 'USD'
-    s2 = 'EUR'
-    print(cal1.getRate(s1,s2))
-    print(cal1.calculate(10.45))
-    print(cal1.getIcon(s2))
+    s2 = 'KRW'
+    cal1 = calcExchange(s1,s2)
+    print(cal1.getRate())
+    print(cal1.calculate(1))
+    print(cal1.getChange('ZAR'))
