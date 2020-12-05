86beab484d34a615da329bc5f72a45c4dc563aa4
Hwna <ha07058@kookmin.ac.kr>
2020-12-05 22:25:59 +0900

    선택한 국가들을 가져오는 showNation 메소드 추가

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from PyQt5.QtWidgets import QLineEdit, QComboBox, QLabel, QPushButton, QGroupBox
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        pass
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
 
 class Exchage(QWidget):
@@ -24,14 +24,17 @@ class Exchage(QWidget):
         self.startBox = QGroupBox('Start')
 
         # 첫 번째 국가를 선택
-        self.firstNation = QComboBox()
+        self.startNation = QComboBox()
+        self.startNation.addItem('1')  # 임시 값
+        self.startNation.addItem('2')
+        self.startNation.addItem('3')
 
         # 환전할 금액을 입력
         self.inputMoney = QLineEdit()
         self.inputMoney.setPlaceholderText('환전할 금액을 입력하세요')
 
         startLayout = QGridLayout()
-        startLayout.addWidget(self.firstNation, 0, 0)
+        startLayout.addWidget(self.startNation, 0, 0)
         startLayout.addWidget(self.inputMoney, 1, 1)
 
         self.startBox.setLayout(startLayout)
@@ -43,7 +46,13 @@ class Exchage(QWidget):
         self.endBox = QGroupBox('End')
 
         # 두 번째 국가를 선택
-        self.secondNation = QComboBox()
+        self.endNation = QComboBox()
+        self.endNation.addItem('1')  # 임시 값
+        self.endNation.addItem('2')
+        self.endNation.addItem('3')
+
+        # 선택한 국가를 전송
+        self.endNation.activated[str].connect(self.showNation)
 
         # 환전한 금액을 표시
         self.displayMoney = QLineEdit()
@@ -51,7 +60,7 @@ class Exchage(QWidget):
         self.displayMoney.setAlignment(Qt.AlignRight)
 
         endLayout = QGridLayout()
-        endLayout.addWidget(self.secondNation, 0, 0)
+        endLayout.addWidget(self.endNation, 0, 0)
         endLayout.addWidget(self.displayMoney, 1, 1)
 
         self.endBox.setLayout(endLayout)
@@ -62,7 +71,9 @@ class Exchage(QWidget):
         mainLayout.addWidget(self.endBox, 1, 0)
         self.setLayout(mainLayout)
 
-        self.show()
+    def showNation(self, nation):
+        print(self.startNation.currentText())
+        print(nation)
 
 
 if __name__ == "__main__":
